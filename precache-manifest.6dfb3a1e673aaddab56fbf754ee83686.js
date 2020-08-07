268a2eed590c6f48e5e8c041a680c8de31f055d7
Karen Astell <KarenAstell@gmail.com>
2020-08-07 11:41:56 -0400

    Updates

---
+++ precache-manifest.6dfb3a1e673aaddab56fbf754ee83686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd1cc63bb7bb853a3612a858e7ef1ec",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "e90e9a039973f3d2d708",
+    "url": "/employee-directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b503ca27af6fc541af48",
+    "url": "/employee-directory/static/css/main.171ebb19.chunk.css"
+  },
+  {
+    "revision": "e90e9a039973f3d2d708",
+    "url": "/employee-directory/static/js/2.dfd21008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.dfd21008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b503ca27af6fc541af48",
+    "url": "/employee-directory/static/js/main.c772c5f4.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
